3b065359c9a2e45f3c13b3a287a25852623dfb21
shamillov <shamillov@bk.ru>
2020-05-16 15:04:12 +0300

    Initial commit

---
+++ app/src/main/java/com/shamilov/payda/ui/FundFragment.kt
@@ -0,0 +1,20 @@
+package com.shamilov.payda.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.shamilov.payda.R
+
+class FundFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_fund, container, false)
+        return root
+    }
+}

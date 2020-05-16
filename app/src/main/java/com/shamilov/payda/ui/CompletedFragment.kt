28711976095250499fc9fe8260a6570b67d43f6c
shamillov <shamillov@bk.ru>
2020-05-16 15:54:53 +0300

    added background color

---
+++ app/src/main/java/com/shamilov/payda/ui/CompletedFragment.kt
@@ -0,0 +1,24 @@
+package com.shamilov.payda.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.shamilov.payda.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CompletedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_completed, container, false)
+    }
+
+}

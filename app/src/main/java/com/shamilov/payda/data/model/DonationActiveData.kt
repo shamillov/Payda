46f243c207096a7cbbb4c0f0ab786c7e476718df
shamillov <shamillov@bk.ru>
2020-05-23 13:43:23 +0300

    added new fields into DonationActiveData

---
+++ app/src/main/java/com/shamilov/payda/data/model/DonationActiveData.kt
@@ -0,0 +1,22 @@
+package com.shamilov.payda.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DonationActiveData (
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val donationTitle: String,
+    @SerializedName("description")
+    val donationDescription: String,
+    @SerializedName("amount")
+    val donationAmount: Int,
+    @SerializedName("region")
+    val fundLocation: String,
+    @SerializedName("fundId")
+    val fundId: Int,
+    @SerializedName("images")
+    val images: List<Image>,
+    @SerializedName("donations")
+    val donationProgress: Int
+)

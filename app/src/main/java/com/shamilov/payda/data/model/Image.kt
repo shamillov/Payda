7be25ce2f9bf6417eb9f0b4d659050f706727915
shamillov <shamillov@bk.ru>
2020-05-23 13:44:50 +0300

    created data class Image

---
+++ app/src/main/java/com/shamilov/payda/data/model/Image.kt
@@ -0,0 +1,18 @@
+package com.shamilov.payda.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Image (
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("size")
+    val size: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("imageContentType")
+    val imageContentType: String,
+    @SerializedName("feeId")
+    val feeId: Int
+)

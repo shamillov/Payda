d143ec5955a7d2435c097e8f603beceb784c7676
shamillov <shamillov@bk.ru>
2020-05-25 16:13:21 +0300

    created RepositoryModule for dagger

---
+++ app/src/main/java/com/shamilov/payda/di/modules/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.shamilov.payda.di.modules
+
+import com.shamilov.payda.data.mapper.DonationMapper
+import com.shamilov.payda.data.remote.api.DonationService
+import com.shamilov.payda.data.repository.DonationRepositoryImpl
+import com.shamilov.payda.domain.repository.DonationRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * Created by Shamilov on 25.05.2020
+ */
+@Module
+class RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideDonationRepository(service: DonationService, mapper: DonationMapper): DonationRepository {
+        return DonationRepositoryImpl(service, mapper)
+    }
+}

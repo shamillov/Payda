33139557bd4230d8258091adeba05af54f759479
shamillov <shamillov@bk.ru>
2020-05-25 16:12:49 +0300

    created NetworkModule for dagger

---
+++ app/src/main/java/com/shamilov/payda/di/modules/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.shamilov.payda.di.modules
+
+import com.google.gson.Gson
+import com.shamilov.payda.data.remote.api.DonationService
+import com.shamilov.payda.utils.const.Const
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by Shamilov on 25.05.2020
+ */
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gsonConverterFactory: GsonConverterFactory,
+                         rxJava2CallAdapterFactory: RxJava2CallAdapterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Const.BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit):DonationService {
+        return retrofit.create(DonationService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory():GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRxJava2CallAdapterFactory(): RxJava2CallAdapterFactory {
+        return RxJava2CallAdapterFactory.create()
+    }
+
+
+}

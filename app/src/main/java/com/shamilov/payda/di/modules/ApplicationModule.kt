e0b57cb751b8ed8e3d40ef4e01a10dc7f676989d
shamillov <shamillov@bk.ru>
2020-05-25 16:12:18 +0300

    created ApplicationModule for dagger

---
+++ app/src/main/java/com/shamilov/payda/di/modules/ApplicationModule.kt
@@ -0,0 +1,27 @@
+package com.shamilov.payda.di.modules
+
+import android.content.Context
+import com.shamilov.payda.domain.executor.SchedulerProvider
+import com.shamilov.payda.domain.executor.SchedulerProviderImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * Created by Shamilov on 25.05.2020
+ */
+@Module
+class ApplicationModule(private val context: Context) {
+
+    @Singleton
+    @Provides
+    internal fun context(): Context {
+        return context
+    }
+
+    @Provides
+    @Singleton
+    fun provideScheduler(): SchedulerProvider {
+        return SchedulerProviderImpl()
+    }
+}
